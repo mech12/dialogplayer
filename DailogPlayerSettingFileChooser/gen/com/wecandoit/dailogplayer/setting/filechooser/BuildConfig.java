04cfe526c7e6dfe612247b8423bcae1fdffb5456
wookrea.cho <icandoit@blueark.com>
2015-02-02 18:31:04 +0900

    a

--- DailogPlayerSettingFileChooser/gen/com/wecandoit/dailogplayer/setting/filechooser/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.wecandoit.dailogplayer.setting.filechooser;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
---
+++ SettingsActivity/src/dentex/youtube/downloader/docs/ChangelogActivity.java
@@ -0,0 +1,14 @@
+package dentex.youtube.downloader.docs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import dentex.youtube.downloader.R;
+
+public class ChangelogActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_changelog);
+	}
+}
