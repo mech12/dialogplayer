a7ef7b2de046c73a6a0b5f1717355547c1aa0c69
wookrea.cho <icandoit@blueark.com>
2015-02-06 19:04:55 +0900

    a

---
+++ jinju_NaviDraw/gen/com/wecandoitgames/jinju_NaviDraw/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wecandoitgames.jinju_NaviDraw;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ jinju_NaviDraw/src/info/androidhive/slidingmenu/PhotosFragment.java
@@ -0,0 +1,22 @@
+package info.androidhive.slidingmenu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.wecandoitgames.jinju_NaviDraw.R;
+
+public class PhotosFragment extends Fragment {
+	
+	public PhotosFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+         
+        return rootView;
+    }
+}
